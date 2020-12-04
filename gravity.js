abb2335c862421456f6e59d0672ffd9763532847
andresbr92 <andresbr92@gmail.com>
2020-12-04 22:06:31 +0100

    second

--- gravity.js
+++ gravity.js
@@ -22,7 +22,6 @@ var Example = Example || {};
         options: {
             width: 800,
             height: 600,
-            showVelocity: true,
             showAngleIndicator: true
         }
     });
@@ -34,31 +33,63 @@ var Example = Example || {};
     Runner.run(runner, engine);
 
     // add bodies
+    var stack = Composites.stack(20, 20, 10, 5, 0, 0, function(x, y) {
+        var sides = Math.round(Common.random(1, 8));
+
+        // triangles can be a little unstable, so avoid until fixed
+        sides = (sides === 3) ? 4 : sides;
+
+        // round the edges of some bodies
+        var chamfer = null;
+        if (sides > 2 && Common.random() > 0.7) {
+            chamfer = {
+                radius: 10
+            };
+        }
+
+        switch (Math.round(Common.random(0, 1))) {
+        case 0:
+            if (Common.random() < 0.8) {
+                return Bodies.rectangle(x, y, Common.random(25, 50), Common.random(25, 50), { chamfer: chamfer });
+            } else {
+                return Bodies.rectangle(x, y, Common.random(80, 120), Common.random(25, 30), { chamfer: chamfer });
+            }
+        case 1:
+            return Bodies.polygon(x, y, sides, Common.random(25, 50), { chamfer: chamfer });
+        }
+    });
+
     World.add(world, [
+        stack,
         Bodies.rectangle(400, 0, 800, 50, { isStatic: true }),
-        Bodies.rectangle(400, 600, 800, 50.5, { isStatic: true }),
+        Bodies.rectangle(400, 600, 800, 50, { isStatic: true }),
         Bodies.rectangle(800, 300, 50, 600, { isStatic: true }),
         Bodies.rectangle(0, 300, 50, 600, { isStatic: true })
     ]);
 
-    engine.world.gravity.y = -1;
-    
-    var stack = Composites.stack(50, 120, 11, 5, 0, 0, function(x, y) {
-        switch (Math.round(Common.random(0, 1))) {
+    // add gyro control
+    if (typeof window !== 'undefined') {
+        var updateGravity = function(event) {
+            var orientation = typeof window.orientation !== 'undefined' ? window.orientation : 0,
+                gravity = engine.world.gravity;
 
-        case 0:
-            if (Common.random() < 0.8) {
-                return Bodies.rectangle(x, y, Common.random(20, 50), Common.random(20, 50));
-            } else {
-                return Bodies.rectangle(x, y, Common.random(80, 120), Common.random(20, 30));
+            if (orientation === 0) {
+                gravity.x = Common.clamp(event.gamma, -90, 90) / 90;
+                gravity.y = Common.clamp(event.beta, -90, 90) / 90;
+            } else if (orientation === 180) {
+                gravity.x = Common.clamp(event.gamma, -90, 90) / 90;
+                gravity.y = Common.clamp(-event.beta, -90, 90) / 90;
+            } else if (orientation === 90) {
+                gravity.x = Common.clamp(event.beta, -90, 90) / 90;
+                gravity.y = Common.clamp(-event.gamma, -90, 90) / 90;
+            } else if (orientation === -90) {
+                gravity.x = Common.clamp(-event.beta, -90, 90) / 90;
+                gravity.y = Common.clamp(event.gamma, -90, 90) / 90;
             }
-        case 1:
-            return Bodies.polygon(x, y, Math.round(Common.random(1, 8)), Common.random(20, 50));
+        };
 
-        }
-    });
-    
-    World.add(world, stack);
+        window.addEventListener('deviceorientation', updateGravity);
+    }
 
     // add mouse control
     var mouse = Mouse.create(render.canvas),
@@ -89,7 +120,13 @@ var Example = Example || {};
     //     runner: runner,
     //     render: render,
     //     canvas: render.canvas,
-        
+    //     stop: function() {
+    //         Matter.Render.stop(render);
+    //         Matter.Runner.stop(runner);
+    //         if (typeof window !== 'undefined') {
+    //             window.removeEventListener('deviceorientation', updateGravity);
+    //         }
+    //     }
     // };
 
 
